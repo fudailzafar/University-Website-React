db1a5731a103ccf1b24bf2cb0b5a9bdf0afaface
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-12-02 06:55:31 +0530

    programs and title components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Programs from "./components/programs/Programs";
+import Title from "./components/title/Title";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title subTitle="Our PROGRAM" title="What We Offer" />
+        <Programs />
+      </div>
     </div>
   );
 };
