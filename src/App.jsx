0571ac269499de472f14b290f88f691f724baade
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-12-02 21:39:41 +0530

    Testimonials slider

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/hero/Hero";
 import Programs from "./components/programs/Programs";
 import Title from "./components/title/Title";
 import About from "./components/about/About";
+import Campus from "./components/campus/campus";
+import Testimonials from "./components/testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -14,6 +16,10 @@ const App = () => {
         <Title subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
+        <Testimonials />
       </div>
     </div>
   );
